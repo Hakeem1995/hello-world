24a1dfab5819b7f4a8c2617599201d0fc16769c4
Hakeem1995 <54942942+Hakeem1995@users.noreply.github.com>
2019-10-09 09:27:19 -0400

    Angle guesser
    f

--- Script.js
+++ Script.js
@@ -2,3 +2,11 @@ function greeting(){
 	var name=prompt("Whats your name");
 	alert("hello"+name);
 }
+
+function thirdangle() {
+
+let angle1 = prompt("give 1st angle");
+let angle2 = prompt("give 2nd angle");
+let angle3 = 180 - angle1-angle2 ;
+alert  (" the third angle is " + angle3);
+}
