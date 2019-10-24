76311d25b2a22f05307d1fb7b3abb75827897671
Hakeem1995 <54942942+Hakeem1995@users.noreply.github.com>
2019-10-24 10:28:33 -0400

    1 q
    r

--- Script.js
+++ Script.js
@@ -10,3 +10,19 @@ let angle2 = prompt("give 2nd angle");
 let angle3 = 180 - angle1-angle2 ;
 alert  (" the third angle is " + angle3);
 }
+
+for (let i=0 ; i<101 ; i++ )
+   if(i%10==0 i%4==0 ) { 
+   	 console.log("tenfour");
+   	}
+   	else if (i%4 ==0){
+   		   console.log("four");
+   		}
+   		else if (i%10 == 0){
+   			console.log("ten");
+   		}
+
+else { 
+	console.log(i)
+}
+
