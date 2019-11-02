85a1ca6f9881ab4d53766bb7a16957cd63663dbe
Hakeem1995 <54942942+Hakeem1995@users.noreply.github.com>
2019-11-02 15:16:51 -0400

    year to century

--- Script.js
+++ Script.js
@@ -11,11 +11,12 @@ let angle3 = 180 - angle1-angle2 ;
 alert  (" the third angle is " + angle3);
 }
 
+function YEARtoCENTURY() {
 let year = prompt("Enter Year");
     let century = year / 100 + 1;
     alert("the century is" + century);
     Math.round(0.5);
-
+}
 
 for (let i=0 ; i<101 ; i++ )
    if(i%10==0 i%4==0 ) { 
