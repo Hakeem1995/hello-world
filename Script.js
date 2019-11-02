0f3416aa4fc556d6fb094974172f85b1f63e1fa1
Hakeem1995 <54942942+Hakeem1995@users.noreply.github.com>
2019-11-02 15:08:56 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -11,6 +11,12 @@ let angle3 = 180 - angle1-angle2 ;
 alert  (" the third angle is " + angle3);
 }
 
+let year = prompt("Enter Year");
+    let century = year / 100 + 1;
+    alert("the century is" + century);
+    Math.round(0.5);
+
+
 for (let i=0 ; i<101 ; i++ )
    if(i%10==0 i%4==0 ) { 
    	 console.log("tenfour");
