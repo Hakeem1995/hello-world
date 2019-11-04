86cfe1584c81eb29ad6fc4ec59c9a3fcc12638e2
Hakeem1995 <54942942+Hakeem1995@users.noreply.github.com>
2019-11-04 10:48:02 -0500

    fixed gh local repo filepath

--- Script.js
+++ Script.js
@@ -1,28 +1,31 @@
+
 function greeting(){
 	var name=  prompt("Whats your name");
 	alert(" hello "+name);
 }
 
 function thirdangle() {
-
-let angle1 = prompt("give 1st angle");
-let angle2 = prompt("give 2nd angle");
-let angle3 = 180 - angle1-angle2 ;
-alert  (" the third angle is " + angle3);
+	let angle1 = prompt("give 1st angle");
+	let angle2 = prompt("give 2nd angle");
+	let angle3 = 180 - angle1-angle2 ;
+	alert (" the third angle is " + angle3);
 }
 
-for (let i=0 ; i<101 ; i++ )
-   if(i%10==0 i%4==0 ) { 
-   	 console.log("tenfour");
-   	}
-   	else if (i%4 ==0){
-   		   console.log("four");
-   		}
-   		else if (i%10 == 0){
-   			console.log("ten");
-   		}
 
-else { 
-	console.log(i)
+function multiplesOfTenAndFour(){
+	for (let i = 0; i <= 100; i++){
+		if(i % 10 == 0 && i % 4 ==0) { 
+			console.log("tenfour");
+		   }
+		else if (i % 4 ==0){
+			console.log("four");
+		 }
+		else if (i%10 == 0){
+			console.log("ten");
+		}
+		
+		else { 
+			console.log(i)
+		}
+	}
 }
-
