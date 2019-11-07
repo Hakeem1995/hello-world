891af2989b96a7d82b9f374f456cbf612e89cb22
Hakeem1995 <54942942+Hakeem1995@users.noreply.github.com>
2019-11-07 08:50:28 -0500

    String lenght and login

--- Script.js
+++ Script.js
@@ -12,40 +12,40 @@ function thirdangle() {
 }
 
 
-function YEARtoCENTURY() {
+// function YEARtoCENTURY() {
 
-let year = prompt("Enter Year");
-    let century = year / 100 + 1;
-    alert("The century is" + century);
-    Math.round(0.5);
-}
+// let year = prompt("Enter Year");
+//     let century = year / 100 + 1;
+//     alert("The century is" + century);
+//     Math.round(0.5);
+// }
 
-for (let i=0 ; i<101 ; i++ ){
-   if(i%10==0 i%4==0 ) { 
-   	 console.log("tenfour");
-   	}
-   	else if (i%4 ==0){
-   		   console.log("four");
-   		}
-   		else if (i%10 == 0){
-   			console.log("ten");
-   		}
-}
+// for (let i=0 ; i<101 ; i++ ){
+//    if(i%10==0 i%4==0 ) { 
+//    	 console.log("tenfour");
+//    	}
+//    	else if (i%4 ==0){
+//    		   console.log("four");
+//    		}
+//    		else if (i%10 == 0){
+//    			console.log("ten");
+//    		}
+// }
 
-function multiplesOfTenAndFour(){
-	for (let i = 0; i <= 100; i++){
-		if(i % 10 == 0 && i % 4 ==0) { 
-			console.log("tenfour");
-		   }
-		else if (i % 4 ==0){
-			console.log("four");
-		 }
-		else if (i%10 == 0){
-			console.log("ten");
-		}
+// function multiplesOfTenAndFour(){
+// 	for (let i = 0; i <= 100; i++){
+// 		if(i % 10 == 0 && i % 4 ==0) { 
+// 			console.log("tenfour");
+// 		   }
+// 		else if (i % 4 ==0){
+// 			console.log("four");
+// 		 }
+// 		else if (i%10 == 0){
+// 			console.log("ten");
+// 		}
 		
-		else { 
-			console.log(i)
-		}
-	}
-}
+// 		else { 
+// 			console.log(i)
+// 		}
+// 	}
+// }
